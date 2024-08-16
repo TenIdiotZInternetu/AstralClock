ba6a0fd6fc8624762094c633c3d507ff73ebf1a2
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-16 19:11:03 +0200

    Removed toUtc minimal function and added toInterval minimal funcion to Clock class. Implemented now and inTime

--- astralclock.hs
+++ astralclock.hs
@@ -1,18 +1,31 @@
 import Data.Time
+import Data.Time.Calendar.OrdinalDate
+import Data.Fixed (Pico)
 
-class Clock t where
-    toUtc :: t -> Maybe UTCTime
-    fromUtc :: UTCTime -> t
+class Clock c where
+    fromUtc :: UTCTime -> IO c
+    toInterval :: c -> NominalDiffTime
 
 
 -- Returns the state off clock at current time
-now :: Clock
+now :: Clock c => IO c
+now = do
+    currentTime <- getCurrentTime
+    fromUtc currentTime
 
 -- Returns the next time the clock will be in the given state
-nextTime :: Clock -> Clock
+-- nextTime :: Clock -> Clock
+-- nextTime 
 
 -- Returns the state of clock after the given time passes
-inTime :: Clock -> Clock
+inTime :: Clock c => c -> IO c
+inTime clock = do
+    currentTime <- getCurrentTime
+    fromUtc $ addUTCTime (toInterval clock) currentTime
 
 
-instance 
+instance Clock UTCTime where
+    fromUtc = return
+    toInterval (UTCTime date seconds) = 
+        let days = fromIntegral $ snd $ toOrdinalDate date :: NominalDiffTime
+        in days * nominalDay + realToFrac seconds

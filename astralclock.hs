282d5fa8cfd8877b350657671fd343b2062b0bdb
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-21 16:13:16 +0200

    Specified *Duration's type as NominalDifftime. Renamed unitInterval to unitDuration and revolutionInterval to intervalDuration.

--- astralclock.hs
+++ astralclock.hs
@@ -4,21 +4,27 @@ import Data.Fixed (Pico)
 import GHC.IO.Encoding (setLocaleEncoding, utf8)
 
 type ReferenceTime = UTCTime
+type ClockWithValue c = (c, Double)
 
+minuteDuration :: NominalDiffTime
 minuteDuration = 60
+
+hourDuration :: NominalDiffTime
 hourDuration = 60 * minuteDuration
+
+dayDuration :: NominalDiffTime
 dayDuration = 24 * hourDuration
 
 class Clock c where
     fromUtc :: c -> UTCTime -> ClockWithValue c
     timeAtZero :: c -> ReferenceTime -> UTCTime
-    unitInterval :: c -> ReferenceTime -> NominalDiffTime
-    revolutionInterval :: c -> ReferenceTime -> NominalDiffTime
+    unitDuration :: c -> ReferenceTime -> NominalDiffTime
+    intervalDuration :: c -> ReferenceTime -> NominalDiffTime
 
 
 toUtc :: Clock c => ClockWithValue c -> ReferenceTime -> UTCTime
 toUtc (clock, value) refTime =
-    let unit = unitInterval clock refTime
+    let unit = unitDuration clock refTime
         zeroTime = timeAtZero clock refTime
         timeOnInterval = toInteger (unit * value)
     in  addUTCTime (timeOnInterval * unit) zeroTime
@@ -44,5 +50,22 @@ next (clock, value) = do
     let timeInCurrentRotation = toUtc (clock, value) curUtcTime
 
     return $ if not doesFullRotation then timeInCurrentRotation
-             else addUTCTime (revolutionInterval clock curUtcTime) curUtcTime
+             else addUTCTime (intervalDuration clock curUtcTime) curUtcTime
 
+
+data CETClock = CETClock
+instance Clock CETClock where
+    fromUtc CETClock (UTCTime day secs) =
+        (CETClock, hour `mod` 12 + 1)
+        where hour = secs / hourDuration
+
+    timeAtZero CETClock (UTCTime refDay refSecs) =
+        UTCTime day (23 * hourDuration)
+        where day = if refSecs > 23 * dayDuration then refDay
+                    else refDay - 1
+
+    unitDuration CETClock _ = hourDuration
+
+    intervalDuration CETClock _ = 24 * hourDuration
+
+    

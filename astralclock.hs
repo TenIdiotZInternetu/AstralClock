70eb0b181695e8608d67e9a61da2e5dd4f4ea280
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-23 18:51:19 +0200

    Defined gears and useful time constants

--- astralclock.hs
+++ astralclock.hs
@@ -6,6 +6,22 @@ import System.Win32 (COORD(yPos))
 
 romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"]
 
+----------------------------- Time Constants ------------------------------
+---------------------------------------------------------------------------
+
+minuteDuration :: NominalDiffTime
+minuteDuration = 60
+
+hourDuration :: NominalDiffTime
+hourDuration = 60 * minuteDuration
+
+dayDuration :: NominalDiffTime
+dayDuration = 24 * minuteDuration
+
+
+-------------------------------- Geometry ---------------------------------
+---------------------------------------------------------------------------
+
 -- Cartesian (x, y) | Polar (radius, angle in radians)
 data Point = Cartesian Float Float | Polar Float Float
 
@@ -16,6 +32,9 @@ data Circle = Circle Point Float
 origin :: Point
 origin = Cartesian 0 0
 
+
+-------------------------------- Astrolabe --------------------------------
+---------------------------------------------------------------------------
 
 cancerTropic :: Circle
 cancerTropic = Circle origin 1
@@ -39,3 +58,30 @@ zodiacRadius = 0.7149
 
 zodiacDistanceFromOrigin :: Float
 zodiacDistanceFromOrigin = 0.2851
+
+
+----------------------------------- Gears -----------------------------------
+-----------------------------------------------------------------------------
+
+-- Gear (revolution duration)
+newtype Gear = Gear NominalDiffTime
+
+angularSpeed :: Gear -> Float
+angularSpeed (Gear revolution) = realToFrac (2 * pi) / realToFrac revolution
+
+
+
+sunGear :: Gear
+sunGear = Gear dayDuration
+
+zodiacGear :: Gear
+zodiacGear = Gear $ 365 / 366 * dayDuration
+
+moonGear :: Gear
+moonGear = Gear $ 378.8 / 365 * dayDuration
+
+moonPhaseGear :: Gear
+moonPhaseGear = Gear $ 29.5305 * dayDuration
+
+calendarGear :: Gear
+calendarGear = Gear $ 365 * dayDuration

2f223e399b1a91c96326c772257ac7fd9f851281
TIZI <tenidiotzinternetudefault@gmail.com>
2024-08-23 18:02:14 +0200

    Defined point and circle

--- astralclock.hs
+++ astralclock.hs
@@ -2,8 +2,16 @@ import Data.Fixed (mod', Pico)
 import Data.Time
 import Data.Time.Calendar.OrdinalDate
 import Prelude hiding (truncate)
+import System.Win32 (COORD(yPos))
 
 romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"]
+
+-- Cartesian (x, y) | Polar (radius, angle in radians)
+data Point = Cartesian Float Float | Polar Float Float
+
+-- Circle (center, radius)
+data Circle = Circle Point Float
+
 
 
 
